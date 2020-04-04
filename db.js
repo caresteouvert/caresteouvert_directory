7e71a625630cdcdce902c127bb93f8ad0789d125
Sylvain Gournay <gournay.sylvain@gmail.com>
2020-04-04 19:13:52 +0200

    Use template and clean code

--- db.js
+++ db.js
@@ -3,20 +3,25 @@ const { Pool } = require('pg');
 const pgPool = new Pool();
 const poiOsmTable = 'poi_osm';
 const normalizedCatColumn = 'normalized_cat';
-const selectDistinctFrom = (column, table) => `SELECT DISTINCT(${column}) FROM ${table};`;
-const selectWhereCategory = (category, table) => `SELECT * FROM ${table} WHERE ${normalizedCatColumn}='${category}' limit 25;`;
-const selectWhereCategoryPage = (category, count, offset, table) => {
-    return `SELECT * FROM ${table} WHERE ${normalizedCatColumn}='${category}' ORDER BY fid LIMIT ${count} OFFSET ${offset};`;
-}
+
+const queries = (table) => {
+    return {
+        selectDistinctFrom: (column) => `SELECT DISTINCT(${column}) FROM ${table};`,
+        selectWhereCategory: (category) => `SELECT * FROM ${table} WHERE ${normalizedCatColumn}='${category}' limit 25;`,
+        selectWhereCategoryPage: (category, count, offset) => {
+            return `SELECT * FROM ${table} WHERE ${normalizedCatColumn}='${category}' ORDER BY fid LIMIT ${count} OFFSET ${offset};`;
+        },
+    }
+};
 
 module.exports = {
     listNormalizedCat: () => {
-        return pgPool.query(selectDistinctFrom(normalizedCatColumn, poiOsmTable))
+        return pgPool.query(queries(poiOsmTable).selectDistinctFrom(normalizedCatColumn))
             .then(res => res.rows.map((row => row[normalizedCatColumn])));
     },
     listEntries: (cat, count, page) => {
         const offset = count * page;
-        return pgPool.query(selectWhereCategoryPage(cat, count, offset, poiOsmTable))
+        return pgPool.query(queries(poiOsmTable).selectWhereCategoryPage(cat, count, offset))
             .then(res => res.rows);
     },
     close: () => {
--- index.js
+++ index.js
@@ -8,56 +8,13 @@ const port = 3000;
 const baseRoute = '/annuaire';
 const elementPerPage = 30;
 
-class Link {
-    href = '';
-    rel = '';
-    type = 'text/html';
-    method = 'GET';
-
-    constructor(href, rel) {
-        this.href = href;
-        this.rel = rel;
-    }
-}
-
-class Annuaire {
-    categories = [];
-    links = [];
-
-    constructor(cats) {
-        this.categories = cats;
-        this.links = cats.map(this.linkFor);
-    }
-
-    linkFor = cat => new Link(`${baseRoute}/${cat}-0`, cat);
-
-    toString = () => JSON.stringify(this, null, 2);
-
-    toHtml = () => {
-        const catLis = _.join(this.categories.map(cat => `<li><a href="${this.linkFor(cat).href}">${cat}</a></li>`), '');
-        const links = _.join(
-            this.links.map(link => {
-                return `<link rel="${link.rel}" href="${link.href}" />`;
-            }), '');
-        return `
-            <html>
-            <head><title>Ca reste ouvert - Catégories des commerces et lieux ouverts durant le confinement</title></head>
-            <body>
-                <div>Les catégories présentes sont :
-                    <ul>${catLis}</ul>
-                </div>
-                <links>${links}</links>
-            </body>
-            </html>
-        `;
-    };
-}
-
 app.get(`${baseRoute}`, function (req, res) {
     db.listNormalizedCat().then(data => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        res.end(new Annuaire(data).toHtml());
+        res.render('annuaire', {
+            cats: data,
+            baseUrl: req.url,
+            postfix: '-0',
+        });
     }).catch(err => {
         res.statusCode = 400;
         res.setHeader('Content-Type', 'application/json');
@@ -66,28 +23,17 @@ app.get(`${baseRoute}`, function (req, res) {
 });
 
 app.get(`${baseRoute}/:category-:page`, function (req, res) {
-    console.log(req.url);
-    console.log(req.params);
-
     const cat = req.params['category'];
     const page = req.params['page'] ? parseInt(req.params['page']) : 0;
     db.listEntries(cat, elementPerPage, page)
         .then(rows => {
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/html');
-            res.end(`
-            <html>
-            <head><title>Ca reste ouvert - POI de type "${cat}" ouverts durant le confinement</title></head>
-            <body>
-                <div>
-                    <ul>
-                        ${ _.join(rows.map(row => `<li>${row.fid} - ${row.name} - ${row.status}</li>`), '')}
-                        ${ (page > 0) ? `<li><a href='${baseRoute}/${cat}-${page - 1}'>Precedent</a></li>` : ''}
-                        ${ (rows.length < elementPerPage) ? '' : `<li><a href='${baseRoute}/${cat}-${page + 1}'>Suivant</a></li>`}
-                    </ul>
-                </div>
-            </body>
-            </html>`);
+            res.render('per-category', {
+                cat: cat,
+                pois: rows,
+                page: page,
+                elementPerPage: elementPerPage,
+                paginatedUrl: `${baseRoute}/${cat}-`,
+            });
         })
         .catch(err => {
             res.statusCode = 400;
@@ -95,6 +41,8 @@ app.get(`${baseRoute}/:category-:page`, function (req, res) {
             res.end(JSON.stringify(err));
         });
 });
+
+app.set('view engine', 'pug');
 
 app.listen(port, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
